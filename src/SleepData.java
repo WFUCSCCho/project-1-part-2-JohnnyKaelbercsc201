db104af39b84a61aa5219958f89e0c9bd5575c43
JohnnyKaelbercsc201 <kaeljn23@wfu.edu>
2024-09-25 21:42:32 -0400

    up to equals method

--- src/SleepData.java
+++ src/SleepData.java
@@ -1,8 +1,8 @@
 public class SleepData implements Comparable<SleepData> {
     //User ID, Age, Gender, Sleep Quality, Bedtime, Wake-up Time, Daily Steps, Calories Burned, Physical Activity, Dietary Habits, Sleep Disorders, Medication Usage
     private Integer userID;
-    private Integer Age;
-    private String Gender;
+    private Integer age;
+    private String gender;
     private Integer sleepQuality;
     private String bedTime;
     private String wakeUpTime;
@@ -16,8 +16,8 @@ public class SleepData implements Comparable<SleepData> {
     //default constructor
     public SleepData() {
         this.userID = null;
-        this.Age = null;
-        this.Gender = null;
+        this.age = null;
+        this.gender = null;
         this.sleepQuality = null;
         this.bedTime = null;
         this.wakeUpTime = null;
@@ -30,10 +30,10 @@ public class SleepData implements Comparable<SleepData> {
     }
 
     //Constructor for Sleep Data
-    public SleepData(Integer userID, Integer Age, String Gender, Integer sleepQuality, String bedTime, String wakeUpTime, Integer dailySteps, Integer caloriesBurned, String physicalActivity, String dietaryHabits, String sleepDisorders, String medicationUsage) {
+    public SleepData(Integer userID, Integer age, String gender, Integer sleepQuality, String bedTime, String wakeUpTime, Integer dailySteps, Integer caloriesBurned, String physicalActivity, String dietaryHabits, String sleepDisorders, String medicationUsage) {
         this.userID = userID;
-        this.Age = Age;
-        this.Gender = Gender;
+        this.age = age;
+        this.gender = gender;
         this.sleepQuality = sleepQuality;
         this.bedTime = bedTime;
         this.wakeUpTime = wakeUpTime;
@@ -46,13 +46,17 @@ public class SleepData implements Comparable<SleepData> {
     }
     @Override
     public int compareTo(SleepData o) {
-        return this.Age.compareTo(o.Age); //compareTo method for comparing Sleepers by Age
+        return this.age.compareTo(o.age); //compareTo method for comparing Sleepers by Age
     }
     @Override
     public String toString() {
-        return "User Id: " +userID + " Age: " +Age + " Gender: " + Gender + " Sleep Quality: " + sleepQuality + " Bed Time: " + bedTime + " Wake-Up Time: " + wakeUpTime + " Daily Steps: " + dailySteps + " Calories Burned: " + caloriesBurned + " Physical Activity: " + physicalActivity + " Dietary Habits: " + dietaryHabits + " Sleep Disorders: " + sleepDisorders + " Medication Usage: " + medicationUsage;
-
+        return "User Id: " +userID + " Age: " +age + " Gender: " + gender + " Sleep Quality: " + sleepQuality + " Bed Time: " + bedTime + " Wake-Up Time: " + wakeUpTime + " Daily Steps: " + dailySteps + " Calories Burned: " + caloriesBurned + " Physical Activity: " + physicalActivity + " Dietary Habits: " + dietaryHabits + " Sleep Disorders: " + sleepDisorders + " Medication Usage: " + medicationUsage;
     }
-
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SleepData sleepData = (SleepData) o;
+        return userID.equals(sleepData.userID);
+    }
 }
